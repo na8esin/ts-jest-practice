c8cd5234cfa31f24d2f64718c9ef4c4b31f3c9a7
Takayuki Watanabe <watanabe@sikmi.com>
2025-03-12 14:20:16 +0900

    コメント追加

--- setupJest.ts
+++ setupJest.ts
@@ -3,6 +3,7 @@ import 'whatwg-fetch';
 
 process.on('unhandledRejection', (err) => {
   // ここでエラーを握りつぶす
+  // 各testの上部とかで、個別に無効、有効を切り替えたいが、それは無理そう
 
   // 下記みたいにログを出力するとjestに怒られる
   // Cannot log after tests are done. Did you forget to wait for something async in your test?
