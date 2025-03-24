1a3cc66ba17f205a77a0b5dac5ac789e7e35d73f
Takayuki Watanabe <watanabe@sikmi.com>
2025-03-24 12:29:04 +0900

    fix comment

--- src/promise/index.test.ts
+++ src/promise/index.test.ts
@@ -1,7 +1,7 @@
 import { sample } from "./index";
 
 it("foo", async () => {
-  // sampleがPromiseを返すわけじゃないので、これだとエラーが補足できない
+  // sampleがPromiseを返すわけじゃないので、↓だとエラーが補足できない
   // await expect(sample()).rejects.toThrow("bar");
 
   sample();
